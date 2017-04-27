51e2e8114e8042d0f17b728228942cb353e39358
Hugo López Fernández <hlfernandez@uvigo.es>
2017-04-27 18:07:23 +0100

    Allow processing several files

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ A collection of some useful `Linux/bash` scripts that I use on a regular basis f
 
 ## List of scripts
 Here is a alphabetically ordered list of the scripts:
-- `clean-trailing-spaces.sh`: this script removes the trailing spaces of a given file passed as argument. Moreover, with option `-b` it also creates a backup of the original file.
+- `clean-trailing-spaces.sh`: this script removes the trailing spaces of a list of given files passed as arguments. Moreover, with option `-b` it also creates a backup of the original file. Example: `clean-trailing-spaces.sh -b file1.txt file2.java`.
 - `launch-aibench.sh`: this script starts an [AIBench](http://www.aibench.org/), with options (`-c/--compile` or `-cc/--clean-compile`) to build it before.
--- clean-trailing-spaces.sh
+++ clean-trailing-spaces.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ $# -gt 2 ] ; then
+if [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
   echo -e "Removes trailing spaces from the specified file.\n"
-  echo "Usage: `basename $0` FILE [OPTION]"
+  echo "Usage: `basename $0` [OPTION] FILEs"
   echo -e "\nOptions:"
   echo -e "\t-b, --backup: creates a backup file"
   exit 0
 fi
 
-FILE=$1;
-
 BACKUP=false
-if [ ! -z "$2" ]; then
-  key="$2"
+if [ ! -z "$1" ]; then
+  key="$1"
 
   case $key in
     -b|--backup)
       BACKUP=true
+      shift
       ;;
-    *)
-      echo "Unknown option: $key" 
-    ;;
   esac
 fi
 
-if [ "$BACKUP" = "true" ]; then
-  sed -i.bak 's/[[:blank:]]*$//' "$FILE"
-else
-  sed -i 's/[[:blank:]]*$//' "$FILE"
-fi
+for FILE in "$@"
+do
+  if [ "$BACKUP" = "true" ]; then
+    sed -i.bak 's/[[:blank:]]*$//' "$FILE"
+  else
+    sed -i 's/[[:blank:]]*$//' "$FILE"
+  fi
+done
